e5736026f092435901bc9c40994d011a99a5e7f1
Vinicius Rosa <vinicius.rosa00@gmail.com>
2020-05-26 23:54:38 -0300

    server works

---
+++ server/server.js
@@ -0,0 +1,30 @@
+const { ApolloServer } = require('apollo-server');
+
+const typeDefs = `
+
+    type Item {
+        id: Int
+        type: String
+        description: String
+    }
+
+    type Query {
+        prefixes: [Item]
+        sufixes: [Item]
+    }
+
+`;
+
+const resolvers = {
+    Query: {
+        prefixes() {
+            return [];
+        },
+        sufixes() {
+            return [];
+        }
+    }
+}
+
+const server = new ApolloServer({ typeDefs, resolvers });
+server.listen();
