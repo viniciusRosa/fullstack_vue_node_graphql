0139361733f06afe1847e3c955da444fcaae99a1
Vinicius Rosa <vinicius.rosa00@gmail.com>
2020-05-26 23:18:10 -0300

    create server

---
+++ client/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div id="slogan" class="text-center">
+      <h1>NameGator</h1>
+      <h6 class="text-secundary">gerador de nome com vue.js, node e graphiql</h6>
+    </div>
+
+    <div id="main">
+      <DomainList></DomainList>
+    </div>
+  </div>
+</template>
+
+<script>
+import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css";
+import DomainList from "./components/DomainList";
+
+export default {
+	name: "app",
+	components: {
+		DomainList
+	}
+};
+</script>
+
+<style>
+#slogan {
+  margin-bottom: 30px;
+  margin-top: 30px;
+}
+#main {
+  background: #f1f1f1;
+  padding-top: 30px;
+  padding-bottom: 30px;
+}
+</style>
